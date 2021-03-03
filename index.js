ba43a577b984ea80f1a5d45cf7d15ebfed1d03b6
Minna Vehniäinen <minna.vehniainen@gmail.com>
2021-03-03 11:17:19 +0200

    heroku

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.static("public"));
+//const server = app.listen(port, () => { .. })
+
 let customers = [
   { id: 1, name: "Jack" },
   { id: 2, name: "Tina" },
 ]; // HTTP GET http://localhost:8080/api/customers
+
 app.get("/api/customers", (req, res) => {
   res.send(customers);
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
