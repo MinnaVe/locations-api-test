60704e8dcdc3cd30873990d75dcb4c42ef34ce59
Minna Vehniäinen <minna.vehniainen@gmail.com>
2021-03-03 11:57:59 +0200

    Lisätty lokaatiot

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ let customers = [
   { id: 2, name: "Tina" },
 ]; // HTTP GET http://localhost:8080/api/customers
 
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
 app.get("/api/customers", (req, res) => {
   res.send(customers);
+});
+
+app.get("/api/locations", (req, res) => {
+  res.json(database);
 });
 
 app.listen(port, () => {
