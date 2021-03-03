8ee8b686cf721f1cf64983bed6063b91858e41d2
Minna Vehniäinen <minna.vehniainen@gmail.com>
2021-03-03 14:21:35 +0200

    Listätty lokaatiot etusivulle

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ app.use(express.static("public"));
 let customers = [
   { id: 1, name: "Jack" },
   { id: 2, name: "Tina" },
-]; // HTTP GET http://localhost:8080/api/customers
+];
 
 let locations = [
   { id: 1, latitude: 60, longitude: 70 },
   { id: 2, latitude: 40, longitude: 80 },
 ];
-
+// HTTP GET http://localhost:8080/api/customers
 app.get("/api/customers", (req, res) => {
   res.send(customers);
 });
 
+// HTTP GET http://localhost:8080/api/locations/1
+app.get("/api/locations/:id([0-9]+)", (req, res) => {
+  let id = Number(req.params.id);
+  let location = locations.find((location) => location.id === id);
+  res.send(location);
+});
+
+// HTTP GET http://localhost:8080/api/locations
 app.get("/api/locations", (req, res) => {
   var str = JSON.stringify(locations, null, 2);
   res.type("application/json");
