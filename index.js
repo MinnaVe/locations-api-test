4917c9a2f67e7c7998dc84f81ff18c04c8b23b91
Minna Vehniäinen <minna.vehniainen@gmail.com>
2021-03-03 12:35:56 +0200

    Muokattu pretty print

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let customers = [
   { id: 2, name: "Tina" },
 ]; // HTTP GET http://localhost:8080/api/customers
 
-let database = [
+let locations = [
   { id: 1, latitude: 60, longitude: 70 },
   { id: 2, latitude: 40, longitude: 80 },
 ];
@@ -19,7 +19,9 @@ app.get("/api/customers", (req, res) => {
 });
 
 app.get("/api/locations", (req, res) => {
-  res.send(database);
+  var str = JSON.stringify(locations, null, 2);
+  res.type("application/json");
+  res.send(str);
 });
 
 app.listen(port, () => {
